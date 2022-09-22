efda61ee9e776eb4646d50ab7ca9f8850568ee7f
sheriffderek <derek@sheriffderek.consulting>
2022-09-22 15:24:37 -0700

    Add spinup and login plugins

---
+++ wp-content/plugins/spinupwp/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.5.x-dev',
+        'version' => '1.5.9999999.9999999-dev',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'spinupwp/spinupwp-plugin',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spinupwp/spinupwp-plugin' => array(
+            'pretty_version' => '1.5.x-dev',
+            'version' => '1.5.9999999.9999999-dev',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
