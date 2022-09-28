7da740a6c4880567586cedff7d5be972de1a39ed
sheriffderek <derek@sheriffderek.consulting>
2022-09-28 16:10:00 -0700

    Show examples of using ACF data in the module templates

--- wp-content/themes/gold-2023/templates/modules/module-seven/template.php
+++ wp-content/themes/gold-2023/templates/modules/module-seven/template.php
@@ -1,33 +1,44 @@
+<?php
+    $intro = get_sub_field('intro'); // 'get' doesn't echo
+
+    // this is a pretty clean way to prepare the variables
+    // and keep the logic outside of the template (as much as possible)
+?>
+
 <section>
     <inner-column>
+
         <module-seven>
             <text-content>
-                <h2>All in the details !!!</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rerum delectus perferendis? Provident optio culpa porro ducimus non dignissimos perspiciatis corporis perferendis. Illo tempore animi ipsam ea ipsa eaque dolorum.</p>
+                <h2><?php the_sub_field('heading'); // 'the' echos ?></h2>
+
+                <p><?=$intro?></p>
             </text-content>
 
             <figure class="one">
                 <picture>
-                    <img src="https://peprojects.dev/images/portrait.jpg">
+                    <img src="https://peprojects.dev/images/portrait.jpg" alt="">
                 </picture>
-                <figcaption>Figure one</figcaption>
+
+                <figcaption>Figure one caption</figcaption>
             </figure>
 
             <figure class="two">
                 <picture>
-                    <img src="https://peprojects.dev/images/portrait.jpg">
+                    <img src="https://peprojects.dev/images/portrait.jpg" alt="">
                 </picture>
-                <figcaption>Figure two</figcaption>
+
+                <figcaption>Figure two caption</figcaption>
             </figure>
 
             <figure class="three">
                 <picture>
-                    <img src="https://peprojects.dev/images/portrait.jpg">
+                    <img src="https://peprojects.dev/images/portrait.jpg" alt="">
                 </picture>
-                <figcaption>Figure three</figcaption>
+
+                <figcaption>Figure three caption</figcaption>
             </figure>
-
-
         </module-seven>
+
     </inner-column>
 </section>
