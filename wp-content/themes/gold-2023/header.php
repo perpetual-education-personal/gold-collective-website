48fb3a5fb82c86f04684fa349d65875088dc0289
Andy <andrew.eliscu@gmail.com>
2022-09-28 17:08:58 -0500

    Update the site description

--- wp-content/themes/gold-2023/header.php
+++ wp-content/themes/gold-2023/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title><?php echo wp_get_document_title(); ?></title>
-    <meta name='description' content="- - -">
+    <meta name='description' content="This is the Gold Collective website.">
     <meta property="og:image" content="https://peprojects.dev/alpha-4/jesse/projects/manga/images/reeds.jpg">
 
     <?php wp_head(); //wp function to insert header components?> 
