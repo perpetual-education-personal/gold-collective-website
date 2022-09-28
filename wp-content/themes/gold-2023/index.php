da39149cb2db9427fc0bc2bd1bec5e3732fa7344
sheriffderek <derek@sheriffderek.consulting>
2022-09-28 14:39:05 -0700

    Formatting

--- wp-content/themes/gold-2023/index.php
+++ wp-content/themes/gold-2023/index.php
@@ -19,23 +19,12 @@
         include('templates/pages/highlights.php');
     }
 
+
     // DETAIL PAGES
 
     if (is_singular('highlight')) {
         echo "Single highlight";
     }
-
-    // if (is_page('artists')) {
-    //     include('templates/pages/artists.php');
-    // }
-
-    // if (is_singular('artist')) {
-    //     echo "Single album";
-    // }
-
-    // if (is_404()) {
-    //     include('templates/pages/page-not-found.php');
-    // }
 
     ?>
 
