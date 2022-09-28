b6d2556b93098cc9e63d5ab3bdf285f9d4144b89
Ned <user@Users-Air.fios-router.home>
2022-09-28 13:35:20 -0400

    Added module 4 & SCSS

--- wp-content/themes/gold-2023/templates/modules/module-four/template.php
+++ wp-content/themes/gold-2023/templates/modules/module-four/template.php
@@ -1,7 +1,42 @@
+
+
 <section>
     <inner-column>
+
         <module-four>
-            <h1 class="attention-voice">??? 4???</h1>
+            <div class='intro'>
+                <h2 class='attention-voice'>Heading for this section</h2>
+                
+                <p>Little leading paragraph would be here.</p>
+            </div>
+
+            <figure class='one'>
+                <picture>
+                    <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+                </picture>
+                <figcaption>
+                    <p>Example figure caption here.</p>
+                </figcaption>
+            </figure>
+            
+            <figure class='two'>
+                <picture>
+                    <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+                </picture>
+                <figcaption>
+                    <p>Example figure caption here.</p>
+                </figcaption>
+            </figure>
+            
+            <figure class='three'>
+                <picture>
+                    <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+                </picture>
+                <figcaption>
+                    <p>Example figure caption here.</p>
+                </figcaption>
+            </figure>
         </module-four>
+
     </inner-column>
 </section>
