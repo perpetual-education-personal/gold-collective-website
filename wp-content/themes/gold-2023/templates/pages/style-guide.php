1a30c93a82325a18c77095e0254d35eeef977f89
Ned <ed.hask85@gmail.com>
2022-09-29 18:11:17 -0400

    Added module 11

--- wp-content/themes/gold-2023/templates/pages/style-guide.php
+++ wp-content/themes/gold-2023/templates/pages/style-guide.php
@@ -6,22 +6,24 @@ include( getFile('loop-module.php') );
 
 
 // How do you choose this file path? $question
-// include('wp-content/themes/gold-2023/templates/modules/module-one/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-one/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-two/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-two/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-three/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-three/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-four/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-four/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-five/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-five/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-six/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-six/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-seven/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-seven/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-eight/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-eight/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-nine/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-nine/template.php');
 
-// include('wp-content/themes/gold-2023/templates/modules/module-ten/template.php');
+include('wp-content/themes/gold-2023/templates/modules/module-ten/template.php');
+
+include('wp-content/themes/gold-2023/templates/modules/module-eleven/template.php');
