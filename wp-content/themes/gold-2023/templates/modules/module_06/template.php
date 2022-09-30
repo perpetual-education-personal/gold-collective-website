0199c4fd14dfd32aac3817945293f238ff01fd25
Ned <ed.hask85@gmail.com>
2022-09-30 17:31:36 -0400

    Updated breakpoints for modules 3, 4 and 6, got rid of repetitive code

--- wp-content/themes/gold-2023/templates/modules/module_06/template.php
+++ wp-content/themes/gold-2023/templates/modules/module_06/template.php
@@ -1,23 +1,20 @@
 <section>
     <inner-column>
-
-        <module-six class='base-template'>
-
+        
+        <module-six class="base-template">
+        
             <div class="module-text">
                 <h2 class="attention-voice">Two become one</h2>
-                <p class="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rerum delectus perferendis? Provident optio culpa porro ducimus non dignissimos perspiciatis corporis perferendis. Illo tempore animi ipsam ea ipsa eaque dolorum.</p>
+                <p class="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rerum delectus perferendis? Provident optio culpa porro ducimus non dignissimos perspiciatis corporis perferendis. Illo tempore animi ipsam ea ipsa eaque dolorum.</p>   
             </div>
-
-            <figure class='one'>
+                
+            <figure class="one">
                 <picture>
-                    <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+                    <img src="https://peprojects.dev/images/portrait.jpg">
                 </picture>
-                <figcaption>
-                    <p>Example figure 1 caption here.</p>
-                </figcaption>
+                <figcaption>Figcaption text.</figcaption>
             </figure>
-
-
+            
         </module-six>
 
     </inner-column>
