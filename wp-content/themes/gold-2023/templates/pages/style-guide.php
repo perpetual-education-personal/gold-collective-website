42cb34d08690ee1e81b7a7887fd7bb7b798cee74
Jesse <jessemerge2@gmail.com>
2022-09-30 16:04:54 -0700

    Remove include files. Loop through row modules now

--- wp-content/themes/gold-2023/templates/pages/style-guide.php
+++ wp-content/themes/gold-2023/templates/pages/style-guide.php
@@ -1,29 +1,4 @@
-<?php 
+<?php
 
 
-include( getFile('loop-module.php') );
-
-
-
-// How do you choose this file path? $question
-include('wp-content/themes/gold-2023/templates/modules/module_01/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_02/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_03/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_04/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_05/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_06/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_07/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_08/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_09/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_10/template.php');
-
-include('wp-content/themes/gold-2023/templates/modules/module_11/template.php');
+include(getFile('loop-module.php'));
