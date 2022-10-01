1278ff7ec53e9c49743b0a7de17ce31b5b280f61
sheriffderek <derek@sheriffderek.consulting>
2022-09-30 19:50:45 -0700

    Set dynamic content for module_01

--- wp-content/themes/gold-2023/templates/modules/module_01/template.php
+++ wp-content/themes/gold-2023/templates/modules/module_01/template.php
@@ -1,75 +1,39 @@
 
-<module-one class='base-template'>
-<<<<<<< HEAD
-
-    <div class='module-text'>
-        <h2 class='attention-voice'>Heading for this section</h2>
-
-        <p class="intro">Little leading paragraph would be here.</p>
-    </div>
-
-    <figure class='one'>
-        <picture>
-            <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo' loading='lazy'>
-        </picture>
-        <figcaption>
-            <p>Example figure caption here.</p>
-        </figcaption>
-    </figure>
-
-    <figure class='two'>
-        <picture>
-            <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo' loading='lazy'>
-        </picture>
-        <figcaption>
-            <p>Example figure caption here.</p>
-        </figcaption>
-    </figure>
-
-    <figure class='three'>
-        <picture>
-            <img src='https://peprojects.dev/images/portrait.jpg' alt='$todo' loading='lazy'>
-        </picture>
-        <figcaption>
-            <p>Example figure caption here.</p>
-        </figcaption>
-    </figure>
 </module-one>
-=======
-	<div class='module-text'>
-		<h2 class='attention-voice'>Heading for this section</h2>
 
-		<p class="intro">Little leading paragraph would be here.</p>
+	<div class='module-text'>
+		<h2 class='attention-voice'><?=$heading?></h2>
+
+		<p class="intro"><?=$intro?></p>
 	</div>
 
 	<figure class='one'>
 		<picture>
-			<img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+			<img src='<?=$imageOne?>' alt='<?=$altTextOne?>'>
 		</picture>
 
 		<figcaption>
-			<p>Example figure caption here.</p>
+			<p><?=$captionOne?></p>
 		</figcaption>
 	</figure>
 
 	<figure class='two'>
 		<picture>
-			<img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+			<img src='<?=$imageTwo?>' alt='<?=$altTextTwo?>'>
 		</picture>
 
 		<figcaption>
-			<p>Example figure caption here.</p>
+			<p><?=$captionTwo?></p>
 		</figcaption>
 	</figure>
 
 	<figure class='three'>
 		<picture>
-			<img src='https://peprojects.dev/images/portrait.jpg' alt='$todo'>
+			<img src='<?=$imageThree?>' alt='<?=$altTextThree?>'>
 		</picture>
 
 		<figcaption>
-			<p>Example figure caption here.</p>
+			<p><?=$captionThree?></p>
 		</figcaption>
 	</figure>
 </module-one>
->>>>>>> ced087c4839c377380d387a843c8b9246f2bb834
