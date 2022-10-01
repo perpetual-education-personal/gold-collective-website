3627871cbbe3c7686a63d2164fa3fed0433a5ce8
sheriffderek <derek@sheriffderek.consulting>
2022-09-30 17:11:26 -0700

    Adjust loop formatting

--- wp-content/themes/gold-2023/loop-module.php
+++ wp-content/themes/gold-2023/loop-module.php
@@ -1,19 +1,17 @@
-<?php
-if (have_rows('page_modules')) {
+<?php if (have_rows('page_modules')) { ?>
 
-  while (have_rows('page_modules')) : the_row();
-
+  <?php while (have_rows('page_modules')) : the_row(); 
     $module = get_row_layout();
-?>
+    ?>
 
     <section>
       <inner-column>
 
         <?php include(getFile("templates/modules/$module/template.php")); ?>
+
       </inner-column>
     </section>
 
-<?php
-  endwhile;
-}
-?>
+  <?php endwhile; ?>
+
+<?php } ?>
