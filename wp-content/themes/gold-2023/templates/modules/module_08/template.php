11a88797b917cdba5b5b483b2cb554e872969b24
Andy <andrew.eliscu@gmail.com>
2022-10-03 15:51:11 -0500

    Update module 8 to use WP get_sub_field variables

--- wp-content/themes/gold-2023/templates/modules/module_08/template.php
+++ wp-content/themes/gold-2023/templates/modules/module_08/template.php
@@ -1,27 +1,24 @@
 <module-eight class="base-template">
 
-    <h1><?=$heading?></h1>
-
-    <h2 class='attention-voice'>What we do</h2>
+    <h2 class='attention-voice'><?=$heading?></h2>
 
 
     <figure class='one'>
         <picture>
-            <img src='https://peprojects.dev/images/landscape.jpg' alt='$todo' loading='lazy'>
+            <img src='<?=$imageOne?>' alt='<?=$altTextOne?>' loading='lazy'>
         </picture>
 
     </figure>
 
     <figcaption>
-        <p>Little story about what this is at 1/334/53</p>
+        <p><?=$captionOne?></p>
     </figcaption>
 
 
     <div class="copy">
-        <p>Photography is a lot like love - you can follow all the rules, but you won't find something truly beautiful unless you go with your gut.<span class="optional"> We draw on our experience and training to capture not just the who/what/where of your day, but the emotions and spirit of it as well. What we won't do is just check shots off a list and then call it a day.</span></p>
-        <p class="optional">Plus, we're fun people to have around.</p>
+        <?=$intro?>
         <cta>
-            <a href="#">Call to Action</a>
+            <?=$cta?>
         </cta>
     </div>
 
