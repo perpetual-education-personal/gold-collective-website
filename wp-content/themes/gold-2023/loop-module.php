79800e9c3db9c83765d1fd698d5d7636b0406cfc
Jesse <jessemerge2@gmail.com>
2022-10-06 15:56:08 -0700

    Add id for routing on page - example in slack

--- wp-content/themes/gold-2023/loop-module.php
+++ wp-content/themes/gold-2023/loop-module.php
@@ -9,7 +9,7 @@
 
 
 
-    <section class='has-<?= $module ?>'>
+    <section class='has-<?= $module ?>' id='<?= $module ?>'>
       <inner-column>
 
         <?php include(getFile("templates/modules/$module/template.php")); ?>
