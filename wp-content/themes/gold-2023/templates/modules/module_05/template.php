4abcc34f50e71551149a6c63fee8b2be278d5d1d
Jesse <jessemerge2@gmail.com>
2022-10-07 12:02:29 -0700

    Module 5 fixes, broken on bigger screen/figcaption

--- wp-content/themes/gold-2023/templates/modules/module_05/template.php
+++ wp-content/themes/gold-2023/templates/modules/module_05/template.php
@@ -1,41 +1,47 @@
 <module-five class='base-template'>
 
     <scroll-container>
-        <figure class='one'>
-            <picture>
-                <img src='<?= $imageOne ?>' alt='<?= $altTextOne ?>' loading='lazy'>
-            </picture>
-            <?php if ($captionOne) { ?>
-                <figcaption>
-                    <p><?= $captionOne ?></p>
-                </figcaption>
+        <ul>
+            <li>
+                <figure class='one'>
+                    <picture>
+                        <img src='<?= $imageOne ?>' alt='<?= $altTextOne ?>' loading='lazy'>
+                    </picture>
+                    <?php if ($captionOne) { ?>
+                        <figcaption>
+                            <p><?= $captionOne ?></p>
+                        </figcaption>
+                    <?php } ?>
+                </figure>
+            </li>
 
-            <?php } ?>
-        </figure>
+            <li>
+                <figure class='two'>
+                    <picture>
+                        <img src='<?= $imageTwo ?>' alt='<?= $altTextTwo ?>' loading='lazy'>
+                    </picture>
+                    <?php if ($captionTwo) { ?>
+                        <figcaption>
+                            <p><?= $captionTwo ?></p>
+                        </figcaption>
+                    <?php } ?>
+                </figure>
+            </li>
 
-        <figure class='two'>
-            <picture>
-                <img src='<?= $imageTwo ?>' alt='<?= $altTextTwo ?>' loading='lazy'>
-            </picture>
-            <?php if ($captionTwo) { ?>
-                <figcaption>
-                    <p><?= $captionTwo ?></p>
-                </figcaption>
-
-            <?php } ?>
-        </figure>
-
-        <figure class='three'>
-            <picture>
-                <img src='<?= $imageThree ?>' alt='<?= $altTextThree ?>' loading='lazy'>
-            </picture>
-            <?php if ($captionThree) { ?>
-                <figcaption>
-                    <p><?= $captionThree ?></p>
-                </figcaption>
-
-            <?php } ?>
-        </figure>
+            <li>
+                <figure class='three'>
+                    <picture>
+                        <img src='<?= $imageThree ?>' alt='<?= $altTextThree ?>' loading='lazy'>
+                    </picture>
+                    <?php if ($captionThree) { ?>
+                        <figcaption>
+                            <p><?= $captionThree ?></p>
+                        </figcaption>
+                    <?php } ?>
+                </figure>
+            </li>
+        </ul>
     </scroll-container>
+
 
 </module-five>
