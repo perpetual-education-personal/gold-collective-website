f4a149a951c91d3d7a554b5ace38327a405cbbb3
Ned <ed.hask85@gmail.com>
2022-10-11 16:45:01 -0400

    Style guide updates #1

--- wp-content/themes/gold-2023/templates/modules/button/template.php
+++ wp-content/themes/gold-2023/templates/modules/button/template.php
@@ -1,5 +1,33 @@
 <div class="generic-button">
 	
-	<a href="<?=$link_url?>"><?=$link_title?></a>
 
-</div>
+<?php
+
+
+
+if( have_rows('actions') ):
+
+    // Loop through rows.
+    while( have_rows('actions') ) : the_row();
+
+        // Load sub field value.
+        $callToAction = get_sub_field('call-to-action');
+	     $link = get_sub_field('link');
+        // Do something...
+
+
+
+        echo "<a href='$link[url]?>'>$callToAction</a>";
+
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif;
+
+
+echo "</div>";
+
--- wp-content/themes/gold-2023/templates/pages/style-guide.php
+++ wp-content/themes/gold-2023/templates/pages/style-guide.php
@@ -1,4 +1,56 @@
+<font-overview>
+
+	<div class="jean-luc">
+	  <h1 class="body-copy">JeanLuc</h1>
+	  <div class="big-example">
+	      <div class="letters">
+	        abcdefghijklm<br>nopqrstuvwxyz      </div>
+	      <div class="symbols">
+	        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+	      <div class="numbers">
+	        12345678910</div>
+	  </div>
+	  <div class="small-example">
+	      <div class="letters">
+	        abcdefghijklm<br>nopqrstuvwxyz      </div>
+	      <div class="symbols">
+	        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+	      <div class="numbers">
+	        12345678910</div>
+	  </div>
+	</div>
+
+	<div class="futura">
+	  <h1 class="body-copy">Futura</h1>
+	  <div class="big-example">
+	      <div class="letters">
+	        abcdefghijklm<br>nopqrstuvwxyz      </div>
+	      <div class="symbols">
+	        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+	      <div class="numbers">
+	        12345678910</div>
+	  </div>
+	  <div class="small-example">
+	      <div class="letters">
+	        abcdefghijklm<br>nopqrstuvwxyz      </div>
+	      <div class="symbols">
+	        !@#$%^&amp;*()_+[]{}: “ ” &lt;&gt;'?,.\/:;</div>
+	      <div class="numbers">
+	        12345678910</div>
+	  </div>
+	</div>
+
+  <div class='example voices'>
+    <div class='title-voice'>Title-voice</div>
+    <div class='attention-voice'>Attention-voice</div>
+    <div class='bold-voice'>Bold-voice</div>
+    <div class='body-copy'>Body-copy</div>
+  </div>
+
+</font-overview>
+
 <?php
+
 
 include(getFile('loop-module.php'));
 
